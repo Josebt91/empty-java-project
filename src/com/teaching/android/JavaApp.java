19b97499c14588f336ab07d87524fe2a23a1365e
Josebt91 <josebt91@hotmail.com>
2018-04-17 13:04:17 +0200

    Mi primer commit con declaración de algunas variables.

--- src/com/teaching/android/JavaApp.java
+++ src/com/teaching/android/JavaApp.java
@@ -6,7 +6,23 @@ package com.teaching.android;
 public class JavaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!"); // Display the string.
+        byte b = 65;
+        char c = 'A';
+        short s = 65;
+        int i = 65;
+        long l = 65L;
+        float f = 65f;
+        double d= 65.55;
+        boolean bol = true;
+
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(bol);// Display the string
     }
 
 }
