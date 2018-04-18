e09e8d42bb7cd931c6e52602663d49b5492d1f99
Josebt91 <josebt91@hotmail.com>
2018-04-18 12:02:24 +0200

    Primeros 20 números pares

--- src/com/teaching/android/JavaApp.java
+++ src/com/teaching/android/JavaApp.java
@@ -10,32 +10,14 @@ public class JavaApp {
 
     public static void main(String[] args) {
 
-        Random generator = new Random();
-        int random = generator.nextInt(10)+1;
-        System.out.print("Programa para adivinar número hasta el 10 en 3 intentos  \n");
 
-        Scanner keyboard = new Scanner (System.in);
-        System.out.print("Introduzca un valor ");
-        int valor = keyboard.nextInt();
-        for(int i=0;i<2;i++) {
-            if (valor == random) {
-                i=3;
-            } else if (valor < random) {
-                System.out.print("Mayor \n");
-                System.out.print("Introduzca nuevo valor ");
-                valor = keyboard.nextInt();
-            } else {
-                System.out.print("Menor \n");
-                System.out.print("Introduzca nuevo valor ");
-                valor = keyboard.nextInt();
+        System.out.print("Programa para mostrar primeros 20 numeros pares  \n");
+        int i=1;
+        for(int j=1;j<=20;i++) {
+            if(i%2==0) {
+                System.out.print(i + " ");
+                j++;
             }
         }
-        if (random == valor) {
-            System.out.print("Correcto ");
-        }
-        else {
-            System.out.print("Fallaste el numero correcto es " + random);
-            }
         }
 }
-
